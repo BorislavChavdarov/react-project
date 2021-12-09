68e5e27eb08a6d3afd197cd702dc5747534709b2
BorislavChavdarov <chavdarov.boby@gmail.com>
2021-12-09 12:57:46 +0200

    set up home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,19 @@
-
-
+import { useEffect, useState } from "react";
+import BookCard from "./BookCard.js";
+import * as bookServices from "../../services/bookServices.js"
 
 const Home = () => {
+
+
+  const [books, setBooks] = useState([]);
+useEffect( async () => {
+bookServices.getAllSortedByDate()
+.then(result => {
+  setBooks(result.slice(0, 3))
+})
+}, [])
+
+
     return ( 
         <div>
     <div className="mySlides w3-display-container w3-center">
@@ -22,36 +34,10 @@ const Home = () => {
   
   <div className="w3-black" id="tour">
     <div className="w3-container w3-content w3-padding-64" style={{"max-width":"800px"}}>
-    
 
-      <div className="w3-row-padding w3-padding-32" style={{margin:"0 -16px"}}>
-        <div className="w3-third w3-margin-bottom">
-          <img src="https://wallpaperaccess.com/full/6875194.jpg" alt="New York" style={{width:"100%"}} className="w3-hover-opacity" />
-          <div className="w3-container w3-white">
-            <p><b>New York</b></p>
-            <p className="w3-opacity">Fri 27 Nov 2016</p>
-            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-            <button className="w3-button w3-black w3-margin-bottom" >Buy Tickets</button>
-          </div>
-        </div>
-        <div className="w3-third w3-margin-bottom">
-          <img src="https://wallpaperaccess.com/full/6875194.jpg" alt="Paris" style={{width:"100%"}} className="w3-hover-opacity" />
-          <div className="w3-container w3-white">
-            <p><b>Paris</b></p>
-            <p className="w3-opacity">Sat 28 Nov 2016</p>
-            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-            <button className="w3-button w3-black w3-margin-bottom" >Buy Tickets</button>
-          </div>
-        </div>
-        <div className="w3-third w3-margin-bottom">
-          <img src="https://wallpaperaccess.com/full/6875194.jpg" alt="San Francisco" style={{width:"100%"}} className="w3-hover-opacity" />
-          <div className="w3-container w3-white">
-            <p><b>San Francisco</b></p>
-            <p className="w3-opacity">Sun 29 Nov 2016</p>
-            <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-            <button className="w3-button w3-black w3-margin-bottom" >Buy Tickets</button>
-          </div>
-        </div>
+    <div className="w3-row-padding w3-padding-32" style={{margin:"0 -16px"}}>
+        
+    {books.map(x => <BookCard key={x._id} book={x} />)}
       </div>
     </div>
   </div>
